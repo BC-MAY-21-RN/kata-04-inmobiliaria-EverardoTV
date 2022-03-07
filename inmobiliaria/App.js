bea2a189d9aaf64ee6ec49f6918d97b85c29aa9a
Everardo Torres <eatv@192.168.1.2>
2022-03-07 15:14:41 -0600

    "fixing the workspace"

--- inmobiliaria/App.js
+++ inmobiliaria/App.js
@@ -1,1 +1,22 @@
-console.log('this is working');
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export default function App(){
+	return(
+		<View style={styles.container}>
+			<Text>Hello Word!</Text>
+		</View>
+	);
+}
+
+
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#fff',
+		alingItems: 'center',
+		justifyContent: 'center',
+		flex: 1,
+	},
+});
